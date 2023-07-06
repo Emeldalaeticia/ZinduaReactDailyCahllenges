65285e5e32acf1eafe7c6e0527ba4a8d72ea6864
Emeldalaeticia <114027865+Emeldalaeticia@users.noreply.github.com>
2023-07-06 17:50:46 +0300

    Hello World challenge

---
+++ challenges/src/App.jsx
@@ -0,0 +1,16 @@
+import Hello from './components/Hello'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="heading-1"> Welcome to my Hello World Application </h1>
+      <p> This is the ultimate way to connect and set-up your bio </p>
+      <Hello name = "World" />
+    </>
+  )
+}
+
+export default App
